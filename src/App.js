c17f13185124a715876a8ada3d675086261d02ec
GoglobalIT <goglobalit2022@gmail.com>
2023-06-10 16:23:05 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './albert-einstein.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          
+          “Imagination is everything. It is the preview of life’s coming attractions.” Albert Einstein
+
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
